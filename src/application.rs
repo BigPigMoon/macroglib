178bd58fb08b457fc893cba95fd70227f360e87e
BigPigMoon <matvey.novikov02@gmail.com>
2024-10-18 16:10:16 +0700

    pub app method

--- src/application.rs
+++ src/application.rs
@@ -7,23 +7,23 @@ pub struct App {
 }
 
 impl App {
-    fn new() -> Self {
+    pub fn new() -> Self {
         App {
             current_scene: None,
         }
     }
 
-    fn set_scene<S: Scene + 'static>(&mut self, scene: S) {
+    pub fn set_scene<S: Scene + 'static>(&mut self, scene: S) {
         self.current_scene = Some(Box::new(scene));
     }
 
-    fn update(&mut self) {
+    pub fn update(&mut self) {
         if let Some(ref mut scene) = self.current_scene {
             scene.update(get_frame_time());
         }
     }
 
-    fn render(&self) {
+    pub fn render(&self) {
         if let Some(ref scene) = self.current_scene {
             scene.render();
         }
